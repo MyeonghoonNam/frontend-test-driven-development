46336bc39df85f9adccca4e8cc0820a84fd2e7f6
MyeonghoonNam <codenmh0822@naver.com>
2024-05-21 20:52:33 +0900

    chore : vite with storybook

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,10 @@
+function TextField() {
+  return (
+    <>
+      <h1>TextField</h1>
+      <input type="text" placeholder="텍스트 필드..." />
+    </>
+  );
+}
+
+export default TextField;
---
+++ src/stories/TextField.stories.tsx
@@ -0,0 +1,11 @@
+import { TextField } from "../components";
+
+export default {
+  title: "TextField",
+  component: TextField,
+};
+
+export const Default = {
+  name: "TextField",
+  render: () => <TextField />,
+};
