1ffdebcd536f859a7e167092d92b4b403d203985
MyeonghoonNam <codenmh0822@naver.com>
2024-05-28 22:57:30 +0900

    chore : vitest 환경 파일 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from 'path';
 
 import { defineConfig } from 'vite';
@@ -6,6 +8,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/utils/setupTest.ts',
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
