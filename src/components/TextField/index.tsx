593880cc9526ab38542dd0c295bb200831adcd5f
MyeonghoonNam <codenmh0822@naver.com>
2024-06-01 22:21:32 +0900

    feat : render test by className

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -18,7 +18,7 @@ const focusedStyle = {
 
 /**
  * placeholder 설정
- * className에 따른 css class 설정
+ * className에 따른 css class 설정 - 테스트 작성 완료
  * 텍스트를 입력할 때마다 onChange 핸들러 호출
  * focus 시 border 스타일 변경
  * focus 시 onFocus 핸들러 호출
---
+++ src/tests/TextField/TextField.spec.tsx
@@ -0,0 +1,24 @@
+import { expect } from 'vitest';
+import { screen } from '@testing-library/react';
+import render from '@/tests/render';
+import { TextField } from '@/components';
+
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  const className = 'test-class';
+
+  // Arrange
+  // className을 지닌 컴포넌트 렌더링
+  // render API 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링 된 DOM 구조 반영
+  // jsDOM: Node.js에서 사용하기 위해 많은 웹 표준을 순수 자바스크립트로 구현
+  await render(<TextField className={className} />);
+
+  // Act
+  // 렌더링에 대한 검증이기에 생략
+  // 렌더링 후 DOM에 해당 class가 존재하는지 검증
+
+  // Assert - 올바른 동작이 실행되었는지 검증
+  // 렌더링 후 DOM에 해당 class가 존재하는지 검증
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요')).toHaveClass(
+    className,
+  );
+});
